a866f39fc277f6cd3b11a157970604e0ba002ff1
lyzs90 <leongyzs@gmail.com>
2017-04-27 21:12:31 +0800

    Clean up CheckoutController

--- frontend/src/app/components/checkout/checkout.component.js
+++ frontend/src/app/components/checkout/checkout.component.js
@@ -5,15 +5,5 @@ angular
     bindings: {
       grandTotal: '<',
       items: '<'
-    },
-    controller: CheckoutController
+    }
   });
-
-/**
- * @name CheckoutController
- * @desc Controller for Stripe form
- * @ngInject
- */
-function CheckoutController() { // TODO: script tag only reads initial parameters once, before angular has time to compile. Should dynamically create the script tag
-
-}
